6a4c3e58ca5a3318b53c9af14eabaf344743d503
Cal <callumburnsoregan@gmail.com>
2026-05-27 15:19:52 +0200

    benches

--- benches/markdown.rs
+++ benches/markdown.rs
@@ -4,9 +4,9 @@ use markdown_wasm::parse_blocks_only; // change this to your crate/module
 
 fn bench_parse_markdown(c: &mut Criterion) {
     let samples = vec![
-        ("small", include_str!("./samples/small.md")),
-        ("medium", include_str!("./samples/medium.md")),
-        ("large", include_str!("./samples/large.md")),
+        ("small", include_str!("../samples/small.md")),
+        ("medium", include_str!("../samples/medium.md")),
+        ("large", include_str!("../samples/large.md")),
     ];
 
     let mut group = c.benchmark_group("parse_markdown");
